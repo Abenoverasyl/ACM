8fd14799c5971d672eb635876cc6c799e90d6930
YAben <abenov.erasyl@gmail.com>
2020-07-28 23:48:49 +0600

    ThreadInheritanceRannable

--- src/ThreadInheritanceRannable.java
+++ src/ThreadInheritanceRannable.java
@@ -6,13 +6,13 @@ public class ThreadInheritanceRannable implements Runnable {
     }
 
     public static void main(String[] args) {
-        ThreadInheritanceRannable rannabele1 = new ThreadInheritanceRannable();
-        ThreadInheritanceRannable rannabele2 = new ThreadInheritanceRannable();
-        ThreadInheritanceRannable rannabele3 = new ThreadInheritanceRannable();
+        ThreadInheritanceRannable runnable1 = new ThreadInheritanceRannable();
+        ThreadInheritanceRannable runnable2 = new ThreadInheritanceRannable();
+        ThreadInheritanceRannable runnable3 = new ThreadInheritanceRannable();
 
-        Thread threadRunnable1 = new Thread(rannabele1);
-        Thread threadRunnable2 = new Thread(rannabele2);
-        Thread threadRunnable3 = new Thread(rannabele3);
+        Thread threadRunnable1 = new Thread(runnable1);
+        Thread threadRunnable2 = new Thread(runnable2);
+        Thread threadRunnable3 = new Thread(runnable3);
 
         threadRunnable1.start();
         threadRunnable2.start();
